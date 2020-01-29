f8d598b425f55cabc81e55437ab194ea64ef0515
Jonathan A. Kollasch <jakllsch@kollasch.net>
2020-01-29 11:46:59 -0600

    superio: add WPCM450 IPMI/KCS logical device number
    Change-Id: Ida7340ae058e5b360c10944111ed07445ee90f04

--- src/superio/nuvoton/wpcm450/wpcm450.h
+++ src/superio/nuvoton/wpcm450/wpcm450.h
@@ -6,6 +6,7 @@
 #define WPCM450_SP2  0x02 /* Com2 */
 #define WPCM450_SP1  0x03 /* Com1 */
 #define WPCM450_KBCK 0x06 /* Keyboard */
+#define WPCM450_KCS  0x11 /* IPMI KCS */
 
 void wpcm450_enable_dev(u8 dev, u16 port, u16 iobase);
 
